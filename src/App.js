b29a2644e6999720d3da39f76fcc82ba037e9c34
Saruna777 <163919425+Saruna777@users.noreply.github.com>
2024-03-27 19:50:43 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import SignInFormDeskto from './SignInFormDeskto';
+
+function App() {
+  return (
+    <div className="App">
+     <SignInFormDeskto/>
+    </div>
+  );
+}
+
+
+
+export default App;
